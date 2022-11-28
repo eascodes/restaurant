ed0e4e2acf5a2e13445c4e846f35e6d0e2404d8a
Emily <easchoettler@gmail.com>
2022-11-28 11:53:59 -0500

    Add menu pics & update photo credits

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,3 +1,17 @@
 export function showMenu() {
     console.log("Here's the menu");
+
+    const content = document.querySelector("#content");
+    const title = document.createElement("h1");
+    const logo = document.createElement("img");
+    const intro = document.createElement("p");
+
+    title.textContent = "MENU";
+    logo.src = "../src/assets/hops-icon.png";
+    intro.textContent = "Stop in and ask about our beers on tap"
+
+    content.appendChild(title);
+    content.appendChild(logo);
+    content.appendChild(intro);
+
 }
