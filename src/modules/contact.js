7dd7fccd057ffba9eca7c67f67efb62754507c34
Emily <easchoettler@gmail.com>
2022-11-29 10:26:56 -0500

    Format contact page

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -4,14 +4,24 @@ export function showContact() {
     const content = document.querySelector("#content");
     const title = document.createElement("h1");
     const logo = document.createElement("img");
-    const intro = document.createElement("p");
+    const addressTitle = document.createElement("h3");
+    const addressText = document.createElement("p");
+    const phone = document.createElement("p");
+    const email = document.createElement("p");
+
+    addressText.classList.add("contact");
 
     title.textContent = "CONTACT";
-    logo.src = "../src/assets/hops-icon.png";
-    intro.textContent = "Brewed from locally grown hops, our beer is ready to knock your socks off. Pair it with one of our warm, crusty pizzas and you'll never wear socks again."
+    logo.src = "../src/assets/bar.jpg";
+    addressTitle.textContent = "VISIT US";
+    addressText.textContent = "123 Brewpub Lane, Ourtown, PA 12345"
+    phone.textContent = "555-654-3211"
+    email.textContent = "stubbsbrewpub@fakeemail.com"
 
     content.appendChild(title);
     content.appendChild(logo);
-    content.appendChild(intro);
-
+    content.appendChild(addressTitle);
+    content.appendChild(addressText);
+    content.appendChild(phone);
+    content.appendChild(email);
 }
