27587d32b21fa398ea2ea34cc8b3014ede8884f8
Emily <easchoettler@gmail.com>
2022-11-29 10:44:58 -0500

    Adjust styling for contact tab

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,19 +1,17 @@
 export function showContact() {
-    console.log("Here's the contact info");
-
     const content = document.querySelector("#content");
     const title = document.createElement("h1");
     const logo = document.createElement("img");
-    const addressTitle = document.createElement("h3");
+    const addressTitle = document.createElement("h2");
     const addressText = document.createElement("p");
     const phone = document.createElement("p");
     const email = document.createElement("p");
 
     addressText.classList.add("contact");
 
-    title.textContent = "CONTACT";
+    title.textContent = "STUBBS BREWPUB";
     logo.src = "../src/assets/bar.jpg";
-    addressTitle.textContent = "VISIT US";
+    addressTitle.textContent = "CONTACT";
     addressText.textContent = "123 Brewpub Lane, Ourtown, PA 12345"
     phone.textContent = "555-654-3211"
     email.textContent = "stubbsbrewpub@fakeemail.com"
