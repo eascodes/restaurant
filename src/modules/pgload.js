18e16f2e0b7ab078ab9cd61d1bdfcd0488a69439
Emily <easchoettler@gmail.com>
2022-11-30 12:40:48 -0500

    Change file path for icon image

--- src/modules/pgload.js
+++ src/modules/pgload.js
@@ -1,3 +1,5 @@
+import Icon from "../assets/hops-icon.png"; 
+ 
  export function loadPage() {
      const content = document.querySelector("#content");
      const title = document.createElement("h1");
@@ -5,7 +7,7 @@
      const intro = document.createElement("p");
 
      title.textContent = "STUBBS BREWPUB";
-     logo.src = "/Users/Cody/repos/portfolio/restaurant/src/assets/hops-icon.png";
+     logo.src = Icon;
      intro.textContent = "Brewed from locally grown hops, our beer is ready to knock your socks off. Pair it with one of our warm, crusty pizzas and you'll never wear socks again."
 
      content.appendChild(title);
