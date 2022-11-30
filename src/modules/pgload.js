727cfadbf9dd3b4afdda0c6b111759e510988411
Emily <easchoettler@gmail.com>
2022-11-30 11:03:39 -0500

    Update photo pathway for main logo

--- src/modules/pgload.js
+++ src/modules/pgload.js
@@ -5,7 +5,7 @@
      const intro = document.createElement("p");
 
      title.textContent = "STUBBS BREWPUB";
-     logo.src = "../src/assets/hops-icon.png";
+     logo.src = "/Users/Cody/repos/portfolio/restaurant/src/assets/hops-icon.png";
      intro.textContent = "Brewed from locally grown hops, our beer is ready to knock your socks off. Pair it with one of our warm, crusty pizzas and you'll never wear socks again."
 
      content.appendChild(title);
